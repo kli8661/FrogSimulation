9541fec4b8bf7613c4d1ac20adc5e278d9b25fcb
BT_1N3_17@bktech.local <BT_1N3_17@bktech.local>
2018-11-19 09:21:08 -0500

    Initial commit

---
+++ src/FrogSimulation.java
@@ -0,0 +1,55 @@
+public class FrogSimulation {
+    private int goalDistance;
+    private int maxHops;
+
+    public FrogSimulation(int dist, int numHops)
+    {
+        goalDistance = dist;
+        maxHops = numHops;
+    }
+
+    private int hopDistance()
+    {
+        return (int)Math.floor(Math.random() * 21) - 5;
+    }
+
+    public boolean simulate()
+    {
+        int d = 0;
+        for(int i = 0; i < maxHops; i++)
+        {
+            d+=hopDistance();
+            if(d > goalDistance)
+            {
+                return true;
+            }
+            else if (d < 0)
+            {
+                return false;
+            }
+        }
+        if(d < goalDistance)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    public double runSimulations(int num)
+    {
+        double yes = 0;
+        for(int i = 0; i < num; i++)
+        {
+            simulate();
+            if(simulate())
+            {
+                yes++;
+            }
+        }
+        System.out.println(yes/num);
+        return (yes/num);
+    }
+}
---
+++ src/Runner.java
@@ -0,0 +1,7 @@
+public class Runner {
+    public static void main(String[] args)
+    {
+        FrogSimulation sim = new FrogSimulation(24,5);
+        sim.runSimulations(10);
+    }
+}
